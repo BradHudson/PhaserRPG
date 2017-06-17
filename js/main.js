15cce899081165575f56c7f1ec0a3fae55641534
Brad Hudson <bradrayhudson@gmail.com>
2017-06-17 17:40:22 -0400

    dont call equip weapon

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@
 		player.bringToTop();
         player.position.x = map.objects.StartPosition[0].x;
         player.position.y = map.objects.StartPosition[0].y;
-        equipWeapon(player, 'Axe'); //wanna see him hold the axe? click to swing
+        //equipWeapon(player, 'Axe'); //wanna see him hold the axe? click to swing
 
 		// add other layers
 		map.createLayer('Foreground');
