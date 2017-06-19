234faf80853596447b36e39297faaa75294936d8
Brad Hudson <bradrayhudson@gmail.com>
2017-06-19 12:48:35 -0400

    some tilesheets and a new level...not sure if it works yet

--- js/loadAssets.js
+++ js/loadAssets.js
@@ -10,10 +10,13 @@ function loadAssetsByStage(stage){
             game.load.spritesheet('limb', 'assets/deku-stick.png', 16, 16);
             game.load.spritesheet('gus', 'assets/gus.png', 30, 32);
             game.load.tilemap('MyTilemap', 'rpgmap.json', null, Phaser.Tilemap.TILED_JSON);
+            game.load.tilemap('TownMap', 'townmap.json', null, Phaser.Tilemap.TILED_JSON);
             game.load.image('grass', 'assets/grass-tiles.png');
             game.load.image('tree', 'assets/tree-tile.png');
             game.load.image('red', 'assets/RED.png');
-            game.load.image('terrain-atlas', 'assets/terrain_atlas.png')
+            game.load.image('terrain-atlas', 'assets/terrain_atlas.png');
+            game.load.image('townTiles1', 'assets/townTiles1.png');
+            game.load.image('townTiles2', 'assets/townTiles2.png');
             game.load.image('speech_part', 'assets/speech_part.png');
             game.load.image('pop', 'assets/pop.png');
             break;
@@ -28,5 +31,13 @@ function addTileMapByStage(stage){
             map.addTilesetImage('tree', 'tree');
             map.addTilesetImage('RED', 'red');
             map.addTilesetImage('terrain-atlas', 'terrain-atlas');
+        case "Town":
+            map = game.add.tilemap('TownMap');
+            map.addTilesetImage('grass', 'grass');
+            map.addTilesetImage('tree', 'tree');
+            map.addTilesetImage('RED', 'red');
+            map.addTilesetImage('terrain-atlas', 'terrain-atlas');
+            map.addTilesetImage('townTiles1', 'townTiles1');
+            map.addTilesetImage('townTiles2', 'townTiles1');
     }
 }
