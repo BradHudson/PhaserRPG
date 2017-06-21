7dbecc34a38249b5b42f39af19c9cb32db5b0726
Brad Hudson <bradrayhudson@gmail.com>
2017-06-21 08:58:42 -0400

    close with states

---
+++ js/bootState.js
@@ -0,0 +1,43 @@
+var bootState = {
+    preload: function(){
+        game.load.spritesheet('dude', 'assets/newguy.png', 30, 32);
+        game.load.spritesheet('adam', 'assets/characters/adam/adam.png', 30, 32);
+        game.load.spritesheet('adam-back', 'assets/characters/adam/adam-back.png', 30, 32);
+        game.load.spritesheet('adam-right', 'assets/characters/adam/adam-right.png', 30, 32);
+        game.load.spritesheet('adam-left', 'assets/characters/adam/adam-left.png', 30, 32);
+        game.load.spritesheet('weapons', 'assets/weapons-sheet.png', 32, 32);
+        game.load.spritesheet('limb', 'assets/deku-stick.png', 16, 16);
+        game.load.spritesheet('gus', 'assets/gus.png', 30, 32);
+        game.load.tilemap('MyTilemap', 'rpgmap.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.tilemap('TownMap', 'townmap.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('grass', 'assets/grass-tiles.png');
+        game.load.image('tree', 'assets/tree-tile.png');
+        game.load.image('red', 'assets/RED.png');
+        game.load.image('terrain-atlas', 'assets/terrain_atlas.png')
+        game.load.image('speech_part', 'assets/speech_part.png');
+        game.load.image('pop', 'assets/pop.png');
+        game.load.image('townTiles1', 'assets/TownTiles1.png');
+        game.load.image('townTiles2', 'assets/townTiles2.png');
+    },
+    create: function() {
+        spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        actionKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+        npcJSONForCurrentStage = loadEnemyStats()[currentStage];
+        game.world.setBounds(0, 0, 1920, 1920);
+        map = game.add.tilemap('MyTileMap');
+        map.addTilesetImage('grass', 'grass');
+        map.addTilesetImage('tree', 'tree');
+        map.addTilesetImage('RED', 'red');
+        map.addTilesetImage('terrain-atlas', 'terrain-atlas');
+		addLayersPlayerCollisions();
+        addNPC();
+        addPlayerAnimations();
+        addTextOnScreen();
+    //    game.state.start('stage1');
+    },
+     update: function(){
+    handleCollisions();
+    resetPlayerVelocity();
+	setKeys();
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer', { preload: preload, create: create, update: update });
+    //var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer', { preload: preload, create: create, update: update });
     var player;
     var spaceKey;
     var actionKey
@@ -10,49 +10,56 @@
     var npcInformation;
     var whoWeTalkingToID;
     var npcInformatonJSON = { Stage1: [] };
-    
-    function preload() {
-        game.load.spritesheet('dude', 'assets/newguy.png', 30, 32);
-        game.load.spritesheet('adam', 'assets/characters/adam/adam.png', 30, 32);
-        game.load.spritesheet('adam-back', 'assets/characters/adam/adam-back.png', 30, 32);
-        game.load.spritesheet('adam-right', 'assets/characters/adam/adam-right.png', 30, 32);
-        game.load.spritesheet('adam-left', 'assets/characters/adam/adam-left.png', 30, 32);
-        game.load.spritesheet('weapons', 'assets/weapons-sheet.png', 32, 32);
-        game.load.spritesheet('limb', 'assets/deku-stick.png', 16, 16);
-        game.load.spritesheet('gus', 'assets/gus.png', 30, 32);
-        game.load.tilemap('MyTilemap', 'rpgmap.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('grass', 'assets/grass-tiles.png');
-        game.load.image('tree', 'assets/tree-tile.png');
-        game.load.image('red', 'assets/RED.png');
-        game.load.image('terrain-atlas', 'assets/terrain_atlas.png')
-        game.load.image('speech_part', 'assets/speech_part.png');
-        game.load.image('pop', 'assets/pop.png');
-        game.load.image('townTiles1', 'assets/TownTiles1.png')
-        game.load.image('townTiles2', 'assets/townTiles2.png')
-    }
-    function create() {
-        spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        actionKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
-        npcJSONForCurrentStage = loadEnemyStats()[currentStage];
-        game.world.setBounds(0, 0, 1920, 1920);
-        map = game.add.tilemap('MyTilemap');
-        map.addTilesetImage('grass', 'grass');
-        map.addTilesetImage('tree', 'tree');
-        map.addTilesetImage('RED', 'red');
-        map.addTilesetImage('terrain-atlas', 'terrain-atlas');
-        map.addTilesetImage('townTiles1', 'townTiles1');
-        map.addTilesetImage('townTiles', 'townTiles2');
-		addLayersPlayerCollisions();
-        addNPC();
-        addPlayerAnimations();
-        addTextOnScreen();
-    }
 
-    function update() {
-    handleCollisions();
-    resetPlayerVelocity();
-	setKeys();
-    }
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
+    game.state.add('boot', bootState);
+    // game.state.add('stage1', stage1State);
+    //game.state.add('town', townState);
+    game.state.start('boot');
+    
+    // function preload() {
+    //     game.load.spritesheet('dude', 'assets/newguy.png', 30, 32);
+    //     game.load.spritesheet('adam', 'assets/characters/adam/adam.png', 30, 32);
+    //     game.load.spritesheet('adam-back', 'assets/characters/adam/adam-back.png', 30, 32);
+    //     game.load.spritesheet('adam-right', 'assets/characters/adam/adam-right.png', 30, 32);
+    //     game.load.spritesheet('adam-left', 'assets/characters/adam/adam-left.png', 30, 32);
+    //     game.load.spritesheet('weapons', 'assets/weapons-sheet.png', 32, 32);
+    //     game.load.spritesheet('limb', 'assets/deku-stick.png', 16, 16);
+    //     game.load.spritesheet('gus', 'assets/gus.png', 30, 32);
+    //     game.load.tilemap('MyTilemap', 'rpgmap.json', null, Phaser.Tilemap.TILED_JSON);
+    //     game.load.tilemap('TownMap', 'townmap.json', null, Phaser.Tilemap.TILED_JSON);
+    //     game.load.image('grass', 'assets/grass-tiles.png');
+    //     game.load.image('tree', 'assets/tree-tile.png');
+    //     game.load.image('red', 'assets/RED.png');
+    //     game.load.image('terrain-atlas', 'assets/terrain_atlas.png')
+    //     game.load.image('speech_part', 'assets/speech_part.png');
+    //     game.load.image('pop', 'assets/pop.png');
+    //     game.load.image('townTiles1', 'assets/TownTiles1.png')
+    //     game.load.image('townTiles2', 'assets/townTiles2.png')
+    // }
+    // function create() {
+    //     spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    //     actionKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    //     npcJSONForCurrentStage = loadEnemyStats()[currentStage];
+    //     game.world.setBounds(0, 0, 1920, 1920);
+    //     map = game.add.tilemap('MyTilemap');
+    //     map.addTilesetImage('grass', 'grass');
+    //     map.addTilesetImage('tree', 'tree');
+    //     map.addTilesetImage('RED', 'red');
+    //     map.addTilesetImage('terrain-atlas', 'terrain-atlas');
+    //     map.addTilesetImage('townTiles1', 'townTiles1');
+    //     map.addTilesetImage('townTiles', 'townTiles2');
+	// 	addLayersPlayerCollisions();
+    //     addNPC();
+    //     addPlayerAnimations();
+    //     addTextOnScreen();
+    // }
+
+    // function update() {
+    // handleCollisions();
+    // resetPlayerVelocity();
+	// setKeys();
+    // }
 
     function handleCollisions(){
         //Collision between player and collision layer
